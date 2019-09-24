653e07a50e9e25307d42267f37855b1a5f9f18f6
Gerardo Alemán <gerardoaleman.mnrz@gmail.com>
2019-09-24 15:06:03 -0500

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM Langua
 ##parser.add_argument('--data', type=str, default='/opt/wp/data/txt/clean',
 
 ## GAM ~ server Paris
-parser.add_argument('--data', type=str, default='/users/aleman/data/train',
+parser.add_argument('--data', type=str, default='/users/aleman/wp/data/train',
                     help='location of the data corpus')
 parser.add_argument('--model', type=str, default='LSTM',
                     help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
